65c908acb3ca49c0753691a9608f3594b84bdd1b
QuietMonkey <taillandierbertrand@gmail.com>
2019-08-21 18:22:46 +0200

    add dependencie howler-react & making some tests

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,55 @@
-import React, {Component} from 'react'
+import React from 'react'
+import ReactHowler from 'react-howler'
 
-class Player extends Component{
+class Player extends React.Component {
+  constructor (props) {
+    super(props)
 
-    render(){
-
-        return(
-            <h1>Test</h1>
-        )
+    this.state = {
+      initialized: false,
+      playing: true
     }
+    this.handlePlay = this.handlePlay.bind(this)
+    this.handlePause = this.handlePause.bind(this)
+  }
+
+  handlePlay () {
+    this.setState({
+      playing: true
+    })
+  }
+
+  handlePause () {
+    this.setState({
+      playing: false
+    })
+  }
+
+  render () {
+    if (this.state.initialized === true) {
+      return (
+        <div>
+          <ReactHowler
+            src='http://goldfirestudios.com/proj/howlerjs/sound.ogg'
+            playing={this.state.playing}
+          />
+          <button onClick={this.handlePlay}>Play</button>
+          <button onClick={this.handlePause}>Pause</button>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <button
+            className='full'
+            onClick={e => this.setState({initialized: true})}
+          >
+            Initialize Auto Player
+          </button>
+        </div>
+      )
+    }
+  }
 }
 
 export default Player
