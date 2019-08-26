ddb6db874817ee696ef74344465f7152309ddbbf
QuietMonkey <taillandierbertrand@gmail.com>
2019-08-26 20:11:31 +0200

    add auth usage in front App

--- src/App.js
+++ src/App.js
@@ -6,22 +6,35 @@ import Albums from './components/Albums';
 
 class App extends Component {
   state = {
+    accessToken: '',
     dataRelease: []
   }
-
-  spotifyApi = new SpotifyWebApi({
-    clientId: '873b1148a2b94911bf9e848a60142cca',
-    clientSecret: '20c5ee21a3004ec7a8078086398df0c9',
-    redirectUri: 'http://localhost:3000'
-  });
 
   componentDidMount() {
     this.setAccessToken()
     this.getReleases(this)
   }
 
+  spotifyApi = new SpotifyWebApi()
+
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    e = r.exec(q)
+    while (e) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+       e = r.exec(q);
+    }
+    return hashParams;
+  }
+
   setAccessToken = () => {
-    this.spotifyApi.setAccessToken('BQCdm9_zAiG4Muh4h4spcwokUy3uzkLusKneqshnbbYyuKRbrgk_ycev_t8fJwaOJ9KcHOFTywMr0lCu0gvENQNA5mD7ChlvCAe756_GVVL2hogZGk8vKafRujAwCqYEA38heqiAugIgprA58RhuiHooy_rXS-yNUCctTj1RJY-whaXt2yRV')
+    const params = this.getHashParams();
+    const token = params.access_token;
+    if (token) {
+      this.spotifyApi.setAccessToken(token);
+    }
   }
 
   getReleases = (objProj) => {
@@ -41,6 +54,7 @@ class App extends Component {
     return (
 
       <div className="App">
+        <a href='http://localhost:8888' > Login to Spotify </a>
         <Albums data={this.state.dataRelease}/>
         <Player />
       </div>
